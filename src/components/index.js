901458c90d8673391d5fde04fc95758af7cd1c9d
alx-cpu <alx.yoi67@gmail.com>
2023-10-30 08:03:26 +0700

    Add Bookmark, Discover, Profile (Layout)

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-import ItemSmall from "./ItemSmall";
-import ListHorizontal from "./ListHorizontal";
-export {ItemSmall, ListHorizontal} 
+import ItemSmall from './ItemSmall'
+import ItemBookmark from './ItemBookmark'
+import ListHorizontal from './ListHorizontal'
+export {ItemSmall, ItemBookmark, ListHorizontal}
